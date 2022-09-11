c04712ac8af88ea9a6a14725c1a3c4be8c25705f
Tdammie <adedigbaadedamola@gmail.com>
2022-09-11 12:24:24 -0700

    Task 8

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - Prints numbers of bas 16 in lowercase
+ *
+ * Return: ALways 0
+ */
+
+int main(0)
+{
+	char c;
+
+	for (c = '0'; c <= '9'; c++)
+	{
+		putchar(c);
+	}
+
+	for (c = 'a'; c <= 'f'; c++)
+	{
+		putchar(c)
+	}
+
+	putchar('\n');
+
+	return (0);
+}
+
+
